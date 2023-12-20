6fe56435526f118591ca213c658f342ee2454595
Nishil <nmvisawadia@gmail.com>
2023-12-20 16:05:53 +0530

    qr-code back cam scan test

--- public/js/qr.js
+++ public/js/qr.js
@@ -1,9 +1,20 @@
-function onScanSuccess(decodedText, decodedResult) {
+// function onScanSuccess(decodedText, decodedResult) {
+//     console.log(`Code scanned = ${decodedText}`, decodedResult);
+//     sessionStorage.setItem('qrdata', decodedText)
+//     window.location.href = './addgadgets.html'
+//     // document.getElementById('output').textContent = decodedText;
+// }
+
+const html5QrCode = new Html5Qrcode("qr-reader");
+const qrCodeSuccessCallback = (decodedText, decodedResult) => {
     console.log(`Code scanned = ${decodedText}`, decodedResult);
-    sessionStorage.setItem('qrdata', decodedText)
-    window.location.href = './addgadgets.html'
-    // document.getElementById('output').textContent = decodedText;
-}
+    document.getElementById('output').textContent = decodedText;
+    /* handle success */
+};
+const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+
+// If you want to prefer back camera
+html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
 
 function setqrdata() {
     const qrdata = JSON.parse(sessionStorage.getItem('qrdata'))
@@ -14,6 +25,6 @@ function setqrdata() {
     document.getElementById('username').setAttribute(value, uname)
 }
 
-var html5QrcodeScanner = new Html5QrcodeScanner(
-    "qr-reader", { fps: 10, qrbox: 250 });
-html5QrcodeScanner.render(onScanSuccess);
+// var html5QrcodeScanner = new Html5QrcodeScanner(
+//     "qr-reader", { fps: 10, qrbox: 250 });
+// html5QrcodeScanner.render(onScanSuccess);
